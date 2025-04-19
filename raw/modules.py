15eaa90c9338999bc357b4053c060d93dcfcdd79
Hhhzj-7 <63787460+Hhhzj-7@users.noreply.github.com>
2025-04-19 15:03:35 +0800

    Update modules.py

--- raw/modules.py
+++ raw/modules.py
@@ -1,16 +1,13 @@
-import torch
-from torch import nn
-
-
 class FunctionPredictor(torch.nn.Module):
     def __init__(self, input_size, output_size, drop_rate=0.1):
         super(FunctionPredictor, self).__init__()
 
-        hidden_size = 4 * output_size
+        hidden_size = math.ceil(input_size/48) * output_size
         self.input_layer = nn.Linear(input_size, hidden_size)
         self.output_layer = nn.Linear(hidden_size, output_size)
         self.relu = nn.ReLU()
         self.dropout = nn.Dropout(p=drop_rate)
+        self.ln1 = nn.LayerNorm(input_size)
         for m in self.modules():
             self.weights_init(m)
 
@@ -21,6 +18,7 @@ class FunctionPredictor(torch.nn.Module):
                 m.bias.data.fill_(0.0)
 
     def forward(self, x):
+        x = self.ln1(x)
         x = self.input_layer(x)
         x = self.relu(x)
         x = self.dropout(x)
